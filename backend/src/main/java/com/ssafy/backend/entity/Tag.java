01d5fb513f0bde84e21251387d413796132754e9
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-07-15 19:49:02 +0900

    Feat: websocket과 stomp를 사용한 채팅 구현

--- backend/src/main/java/com/ssafy/backend/entity/Tag.java
+++ backend/src/main/java/com/ssafy/backend/entity/Tag.java
@@ -3,7 +3,10 @@ package com.ssafy.backend.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 @Getter
