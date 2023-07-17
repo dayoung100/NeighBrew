724d207422228fe30e96d762044bbf0bbe0955af
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-07-17 17:12:14 +0900

    Feat: entity 수정 및 soft delete 적용

--- backend/src/main/java/com/ssafy/backend/entity/Drink.java
+++ backend/src/main/java/com/ssafy/backend/entity/Drink.java
@@ -2,26 +2,46 @@ package com.ssafy.backend.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Where;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
+@Where(clause = "deleted = false")
 public class Drink {
     @Id
     @GeneratedValue
     private Long drinkId;
 
-    @Column(nullable = false, unique = true, length = 20)
+    @Column(nullable = false, unique = true, length = 50)
     private String name;
 
-    @Column(nullable = false, columnDefinition = "varchar(255) default 'no image'")
+    @Lob
+    @Column(nullable = false, columnDefinition = "TEXT default 'no image'")
     private String image;
 
-    @Column(nullable = false, columnDefinition = "float default 0.0")
+    @Column(nullable = false, columnDefinition = "decimal(3, 2) default 0.0")
     private Float degree;
 
     @Lob
     private String description;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.image = this.image == null ? "no image" : this.image;
+        this.degree = this.degree == null ? 0.0f : this.degree;
+    }
+
+    private boolean deleted = false;
 }
--- backend/src/main/java/com/ssafy/backend/entity/User.java
+++ backend/src/main/java/com/ssafy/backend/entity/User.java
@@ -2,13 +2,18 @@ package com.ssafy.backend.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Where;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
 @Getter
 @Setter
+@Where(clause = "deleted = false")
 public class User {
     @Id
     @GeneratedValue
@@ -36,9 +41,17 @@ public class User {
     @Lob
     private String intro;
 
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
     @Column(nullable = false, columnDefinition = "float default 40.0")
     private Float iu;
 
     @Column(nullable = false, columnDefinition = "varchar(255) default 'no image'")
     private String profile;
+
+    private boolean deleted = false;
 }
