108f93bcb29dd8821cad3bded1f38c3c8fadf16e
강동혁 <ztarhyuk127@gmail.com>
2023-07-17 17:59:40 +0900

    UI. navbar and footer

---
+++ Front/src/UI/drinkpost/DrinkpostMain.tsx
@@ -0,0 +1,7 @@
+// import axios from 'axios'
+
+const drinkpost = () => {
+  return <h1>Drinkpost</h1>;
+};
+
+export default drinkpost;
--- Front/src/UI/user/MyPage.tsx
+++ Front/src/UI/user/MyPage.tsx
@@ -1,10 +1,10 @@
-// 마이 페이지
-import { useState } from 'react';
-const MyPage = props => {
-  const [userData, setUserData] = useState({}); // 유저 정보
-  return (
-    <div>
-      <h1>MyPage</h1>
-    </div>
-  );
-};
+// // 마이 페이지
+// import { useState } from 'react';
+// const MyPage = (props) => {
+//   const [userData, setUserData] = useState({}); // 유저 정보
+//   return (
+//     <div>
+//       <h1>MyPage</h1>
+//     </div>
+//   );
+// };
