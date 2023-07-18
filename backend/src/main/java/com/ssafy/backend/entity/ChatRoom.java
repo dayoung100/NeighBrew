f79753710116271111c9239abb4206445611bb2d
인영교 <yeongkyo1997@gmail.com>
2023-07-18 12:22:21 +0900

    Revert "Merge branch 'feature/be/chat' into 'dev'"
    This reverts merge request !18

--- backend/src/main/java/com/ssafy/backend/entity/ChatRoom.java
+++ backend/src/main/java/com/ssafy/backend/entity/ChatRoom.java
@@ -3,14 +3,19 @@ package com.ssafy.backend.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Where;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
+@Where(clause = "deleted = false")
 public class ChatRoom {
     @Id
     @GeneratedValue
@@ -19,9 +24,17 @@ public class ChatRoom {
     @Column(nullable = false, length = 100)
     private String chatRoomName;
 
-    @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL)
-    private List<ChatMessage> messages;
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "chatRoomUser", cascade = CascadeType.ALL)
+    private List<ChatRoomUser> chatRoomUsers;
 
     @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL)
     private List<ChatRoomUser> users;
+
+    private boolean deleted = false;
 }
