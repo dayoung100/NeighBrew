7c41bd3f1ce2f135f0a64ae481230ef6b92f756c
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-07-18 11:27:13 +0900

    Fix: 채팅 버그 픽스, 예전 코드 다시 가져옴

--- backend/src/main/java/com/ssafy/backend/entity/ChatRoomUser.java
+++ backend/src/main/java/com/ssafy/backend/entity/ChatRoomUser.java
@@ -2,28 +2,16 @@ package com.ssafy.backend.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.Where;
-import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
-@Where(clause = "deleted = false")
 public class ChatRoomUser {
     @Id
     @GeneratedValue
     private Long chatRoomUserId;
-
-    @Lob
-    private String message;
-
-    private boolean isImg;
-
-    @CreatedDate
-    private LocalDateTime createdAt;
 
     @ManyToOne
     @JoinColumn(name = "chatRoomId")
@@ -32,6 +20,4 @@ public class ChatRoomUser {
     @ManyToOne
     @JoinColumn(name = "userId")
     private User user;
-
-    private boolean deleted = false;
 }
