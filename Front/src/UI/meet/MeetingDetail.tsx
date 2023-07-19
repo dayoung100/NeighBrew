ee16c92f89c9e4accee22b49a0d9ea0ecb157e28
dayounglee <53906423+dayoung100@users.noreply.github.com>
2023-07-19 17:47:56 +0900

    Feat : MeetingDetail 추가, UI 배치중

--- Front/src/UI/meet/MeetingDetail.tsx
+++ Front/src/UI/meet/MeetingDetail.tsx
@@ -1,8 +1,21 @@
+import { arrowLeftIcon } from "../../assets/AllIcon";
+
 const MeetingDetail = () => {
+  const ArrowLeftIcon = arrowLeftIcon("black");
+
   return (
     <div>
-      미팅 상세 페이지
-      <div>모임정보</div>
+      <div>
+        <div style={{ width: "100%", textAlign: "left" }}>
+          {ArrowLeftIcon}
+          뒤로가기 버튼과 태그
+        </div>
+        <div>모임의 제목이 들어갑니다</div>
+        <div>주최자 이름이 들어갑니다</div>
+        <div>인원정보가 여기에</div>
+      </div>
+      <div>자세한 정보는 여기에, radius를 줘야</div>
+      <div>모임 예정 장소와 시간, z-index올리고 position absolute로</div>
     </div>
   );
 };
