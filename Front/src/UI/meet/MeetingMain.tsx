4c7479601a9deff580087bf2e5c371c258fae7fe
dayounglee <53906423+dayoung100@users.noreply.github.com>
2023-07-19 15:11:56 +0900

    Feat : 내모임페이지 구현 및 상하단 바 추가

--- Front/src/UI/meet/MeetingMain.tsx
+++ Front/src/UI/meet/MeetingMain.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import styled from "styled-components";
 import MeetingMy from "./MeetingMy";
 import MeetingFind from "./MeetingFind";
+import NavbarWithoutSearch from "../navbar/NavbarWithoutSearch";
+import Footer from "../footer/Footer";
 
 const TopMenu = styled.div`
   height: 2rem;
@@ -12,14 +14,15 @@ const TopMenu = styled.div`
 `;
 
 const TopMenuDetail = styled.button<{ isFocused: boolean }>`
-  color: var(--${props => (props.isFocused ? "c-black" : "c-gray")});
+  color: var(--${(props) => (props.isFocused ? "c-black" : "c-gray")});
   font-family: "JejuGothic";
   font-size: 20px;
   line-height: 150%;
   padding: 0 1rem;
   outline: none;
   border: none;
-  border-bottom: ${props => (props.isFocused ? "2px solid var(--c-black);" : "none;")};
+  border-bottom: ${(props) =>
+    props.isFocused ? "2px solid var(--c-black);" : "none;"};
   background: white;
 `;
 
@@ -28,15 +31,27 @@ const meetingMain = () => {
 
   return (
     <div>
+      <header>
+        <NavbarWithoutSearch />
+      </header>
       <TopMenu>
-        <TopMenuDetail isFocused={selectedMenu === "find"} onClick={() => setSelectedMenu("find")}>
+        <TopMenuDetail
+          isFocused={selectedMenu === "find"}
+          onClick={() => setSelectedMenu("find")}
+        >
           모임찾기
         </TopMenuDetail>
-        <TopMenuDetail isFocused={selectedMenu === "my"} onClick={() => setSelectedMenu("my")}>
+        <TopMenuDetail
+          isFocused={selectedMenu === "my"}
+          onClick={() => setSelectedMenu("my")}
+        >
           내모임
         </TopMenuDetail>
       </TopMenu>
       <div>{selectedMenu === "find" ? <MeetingFind /> : <MeetingMy />}</div>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 };
