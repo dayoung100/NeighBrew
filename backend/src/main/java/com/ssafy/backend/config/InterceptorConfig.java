1ebe76a96903366477cf02ef133f9d1d765981e4
wnstj7788 <wnstj7788@gmail.com>
2023-07-20 11:15:05 +0900

    Interecptor down

--- backend/src/main/java/com/ssafy/backend/config/InterceptorConfig.java
+++ backend/src/main/java/com/ssafy/backend/config/InterceptorConfig.java
@@ -8,19 +8,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
-
-    private final JwtTokenInterceptor jwtTokenInterceptor;
-
-    @Autowired
-    public InterceptorConfig(JwtTokenInterceptor jwtTokenInterceptor) {
-        this.jwtTokenInterceptor = jwtTokenInterceptor;
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        // JWT 토큰 인터셉터 등록
-        registry.addInterceptor(jwtTokenInterceptor)
-
-                .excludePathPatterns("/api/auth/**");  // 제외할 URL 패턴 지정 (로그인 등 예외적으로 인증이 필요하지 않은 URL)
-    }
+//
+//    private final JwtTokenInterceptor jwtTokenInterceptor;
+//
+//    @Autowired
+//    public InterceptorConfig(JwtTokenInterceptor jwtTokenInterceptor) {
+//        this.jwtTokenInterceptor = jwtTokenInterceptor;
+//    }
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        // JWT 토큰 인터셉터 등록
+//        registry.addInterceptor(jwtTokenInterceptor)
+//
+//                .excludePathPatterns("/api/auth/**");  // 제외할 URL 패턴 지정 (로그인 등 예외적으로 인증이 필요하지 않은 URL)
+//    }
 }
