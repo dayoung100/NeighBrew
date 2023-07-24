1e57e30ed817f3ab8e37faecb148e891841b0073
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-07-24 16:29:57 +0900

    Fix: Authorization cors에러 해결

--- backend/src/main/java/com/ssafy/backend/config/WebMvcConfig.java
+++ backend/src/main/java/com/ssafy/backend/config/WebMvcConfig.java
@@ -20,8 +20,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
+                .allowedHeaders("Authorization") // Allow Authorization header
+                .maxAge(3000);
     }
 }
