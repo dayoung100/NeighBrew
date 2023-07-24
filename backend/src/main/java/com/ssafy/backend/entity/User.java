cab72f0000a922375be0180eb242c917fff852e6
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-07-24 14:10:13 +0900

    Fix: user entity에서 @Setter를 제거하고 생성자를 update와 create를 수정

--- backend/src/main/java/com/ssafy/backend/entity/User.java
+++ backend/src/main/java/com/ssafy/backend/entity/User.java
@@ -1,6 +1,7 @@
 package com.ssafy.backend.entity;
 
 import com.ssafy.backend.authentication.domain.oauth.OAuthProvider;
+import com.ssafy.backend.dto.UserUpdateDto;
 import lombok.*;
 import org.hibernate.annotations.Where;
 import org.springframework.data.annotation.CreatedDate;
@@ -13,36 +14,33 @@ import java.util.List;
 
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Where(clause = "deleted = false")
 public class User {
-    // 최조 로그인 시 기본 정보를 가져와야함
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
-    @Column(nullable = true, unique = true, length = 50)
+    @Column(nullable = false, unique = true, length = 50)
     private String email;
 
-    @Column(nullable = true, unique = true, length = 20)
+    @Column(nullable = false, unique = true, length = 20)
     private String nickname;
 
-    @Column(nullable = true)
+    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private OAuthProvider oAuthProvider;
 
-    @Column(nullable = true, length = 20)
+    @Column(nullable = false, length = 20)
     private String name;
 
-    @Column(nullable = true, length = 100)
+    @Column(nullable = false, length = 100)
     private String password;
 
-    @Column(nullable = true, unique = true, length = 20)
+    @Column(nullable = false, unique = true, length = 20)
     private String phone;
 
     @Temporal(TemporalType.DATE)
-    @Column(nullable = true)
+    @Column(nullable = false)
     private Date birth;
 
 
@@ -58,10 +56,14 @@ public class User {
     @Column(nullable = false, columnDefinition = "float default 40.0")
     private Float liverPoint;
 
-    @Column(nullable = true, columnDefinition = "varchar(255) default 'no image'")
+    @Column(nullable = false, columnDefinition = "varchar(255) default 'no image'")
     private String profile;
 
-    private boolean deleted = false;
+    @PrePersist
+    public void prePersist() {
+        this.liverPoint = 40.0f;
+        this.profile = "no image";
+    }
 
 
     @Builder
@@ -70,5 +72,35 @@ public class User {
         this.nickname = nickname;
         this.name = name;
         this.oAuthProvider = oAuthProvider;
+    }
+
+    @Builder
+    public User(Long userId, String email, String nickname, OAuthProvider oAuthProvider, String name, String password,
+                String phone, Date birth, String intro, Float liverPoint, String profile) {
+        this.userId = userId;
+        this.email = email;
+        this.nickname = nickname;
+        this.oAuthProvider = oAuthProvider;
+        this.name = name;
+        this.password = password;
+        this.phone = phone;
+        this.birth = birth;
+        this.intro = intro;
+        this.liverPoint = liverPoint;
+        this.profile = profile;
+    }
+    public void updateFromDto(UserUpdateDto updateDto) {
+        if (updateDto.getNickname() != null) {
+            this.nickname = updateDto.getNickname();
+        }
+        if (updateDto.getIntro() != null) {
+            this.intro = updateDto.getIntro();
+        }
+        if (updateDto.getProfile() != null) {
+            this.profile = updateDto.getProfile();
+        }
+        if (updateDto.getLiverPoint() != null) {
+            this.liverPoint = updateDto.getLiverPoint();
+        }
     }
 }
