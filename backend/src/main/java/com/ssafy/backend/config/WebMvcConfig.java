317e355aa28eba2466b3f9c54a8f4f74868e4f0b
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-07-25 14:29:28 +0900

    Fix: Cors에러 해결

--- backend/src/main/java/com/ssafy/backend/config/WebMvcConfig.java
+++ backend/src/main/java/com/ssafy/backend/config/WebMvcConfig.java
@@ -21,6 +21,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 }
