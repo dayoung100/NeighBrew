217a4e80ec8e6d1ed075116438a2f4d740a4993c
wnstj7788 <wnstj7788@gmail.com>
2023-07-25 15:33:28 +0900

    fix : userFollwer

--- backend/src/main/java/com/ssafy/backend/service/UserService.java
+++ backend/src/main/java/com/ssafy/backend/service/UserService.java
@@ -1,23 +1,42 @@
 package com.ssafy.backend.service;
 
 import com.ssafy.backend.dto.UserUpdateDto;
+import com.ssafy.backend.entity.Follow;
 import com.ssafy.backend.entity.User;
+import com.ssafy.backend.repository.FollowRepository;
 import com.ssafy.backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final FollowRepository followRepository;
 
     @Value("${oauth.kakao.client-id}")
     private String clientId;
 
     @Value("${oauth.kakao.url.auth}")
     private String authUrl;
+
+
+    public Long getFollowerCount(Long userId) {
+        Long followerCount = (long) followRepository.findFollowsByFollowing_UserId(userId).size();
+        return followerCount;
+    }
+
+    public Long getFollowingCount(Long userId) {
+        Optional<List<Follow>> aa = followRepository.findByFollowing_UserId(userId);
+        System.out.println("aa.toString() = " + aa.toString());
+      Long folloingCount = followRepository.findByFollowing_UserId(userId).stream().count();
+      return  folloingCount;
+    }
 
     public String redirectApiUrl() {
         String redirectUri = "http://localhost:8080/kakao/callback";
