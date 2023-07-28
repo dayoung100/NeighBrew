4420cdf8a3b64e676097e27d3bfff449eb1bef4b
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-07-28 17:54:00 +0900

    Refactor: Follow기능 수정 - 유저 아이디로 팔로워 조회

--- backend/src/main/java/com/ssafy/backend/controller/FollowController.java
+++ backend/src/main/java/com/ssafy/backend/controller/FollowController.java
@@ -36,7 +36,7 @@ public class FollowController {
     @GetMapping("/{userId}")
     public ResponseEntity<List<Follow>> getFollowers(@PathVariable Long userId) {
 
-        List<Follow> followers = followService.getFollowers(Long.valueOf(userId));
+        List<Follow> followers = followService.getFollowers(userId);
         return ResponseEntity.ok(followers);
     }
 
