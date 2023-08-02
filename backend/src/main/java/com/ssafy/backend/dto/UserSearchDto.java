28144744e020c8dd4f17b8719fc19760d99e9a35
wnstj7788 <wnstj7788@gmail.com>
2023-08-02 15:22:19 +0900

    feature : searchUsersByUserName

---
+++ backend/src/main/java/com/ssafy/backend/dto/UserSearchDto.java
@@ -0,0 +1,15 @@
+package com.ssafy.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserSearchDto {
+    private String NickName;
+
+}
