38b12a1d0b7429a61b8fccc546d5b97f3ed9037e
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-08-02 19:33:57 +0900

    Refactor: 채팅방의 컨트롤러와 서비스 로직 분리 - ChatRommController에서 ChatRoomService로 이동하여 관심사를 분리 - ChatRoomController에서는 HTTP 요청 및 응답을 처리하는 것에만 집중 - ChatRoomService는 채팅방 생성과 유저의 채팅방 조회에 관련된 모든 비즈니스 로직을 처리 - ChatRoomService에서 여러 repository 사용

--- backend/src/main/java/com/ssafy/backend/controller/ChatRoomController.java
+++ backend/src/main/java/com/ssafy/backend/controller/ChatRoomController.java
@@ -1,22 +1,12 @@
 package com.ssafy.backend.controller;
 
-import com.ssafy.backend.entity.ChatMessage;
 import com.ssafy.backend.entity.ChatRoom;
-import com.ssafy.backend.entity.ChatRoomUser;
-import com.ssafy.backend.entity.User;
-import com.ssafy.backend.repository.ChatMessageRepository;
-import com.ssafy.backend.repository.ChatRoomRepository;
-import com.ssafy.backend.repository.ChatRoomUserRepository;
-import com.ssafy.backend.repository.UserRepository;
 import com.ssafy.backend.service.ChatRoomService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.transaction.Transactional;
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -24,42 +14,13 @@ import java.util.Map;
 @RequiredArgsConstructor
 @RequestMapping("/api/chatroom")
 public class ChatRoomController {
-    private final ChatRoomRepository chatRoomRepository;
-    private final UserRepository userRepository;
-    private final ChatRoomUserRepository chatRoomUserRepository;
-    private final ChatMessageRepository chatMessageRepository;
     private final ChatRoomService chatRoomService;
 
 
     // 채팅방 생성
-    @Transactional
     @PostMapping("/room")
     public ResponseEntity<ChatRoom> createChatRoom(@RequestBody Map<String, Object> map) {
-        ChatRoom room = ChatRoom.builder()
-                .chatRoomName((String) map.get("name"))
-                .build();
-        List<Integer> userIdList = (List<Integer>) map.get("userIdList");
-
-        log.info("userIdList: {}", userIdList);
-        for (Integer userId : userIdList) {
-            User user = userRepository.findById(Long.valueOf(userId)).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
-            log.info("user: {}", user);
-            ChatRoomUser chatRoomUser = ChatRoomUser.builder()
-                    .chatRoom(room)
-                    .user(user)
-                    .build();
-            log.info("chatRoomUser: {}", chatRoomUser);
-            chatRoomUserRepository.save(chatRoomUser);
-        }
-
-        chatRoomRepository.save(room);
-        chatMessageRepository.save(ChatMessage.builder()
-                .chatRoom(room)
-                .user(null)
-                .message("채팅방이 생성되었습니다.")
-                .timestamp(LocalDateTime.now())
-                .build());
-        return ResponseEntity.ok(room);
+        return ResponseEntity.ok(chatRoomService.createChatRoom(map));
     }
 
     // 유저 아이디로 채팅방 조회
--- backend/src/main/java/com/ssafy/backend/service/ChatRoomService.java
+++ backend/src/main/java/com/ssafy/backend/service/ChatRoomService.java
@@ -1,23 +1,62 @@
 package com.ssafy.backend.service;
 
+import com.ssafy.backend.entity.ChatMessage;
 import com.ssafy.backend.entity.ChatRoom;
 import com.ssafy.backend.entity.ChatRoomUser;
+import com.ssafy.backend.entity.User;
+import com.ssafy.backend.repository.ChatMessageRepository;
+import com.ssafy.backend.repository.ChatRoomRepository;
 import com.ssafy.backend.repository.ChatRoomUserRepository;
+import com.ssafy.backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ChatRoomService {
+    private final ChatMessageRepository chatMessageRepository;
+    private final ChatRoomRepository chatRoomRepository;
     private final ChatRoomUserRepository chatRoomUserRepository;
+    private final UserRepository userRepository;
+
 
     public List<ChatRoom> findUserChatRooms(Long userId) {
         return chatRoomUserRepository.findByUser_UserId(userId)
                 .stream()
                 .map(ChatRoomUser::getChatRoom)
                 .collect(Collectors.toList());
+    }
+
+    public ChatRoom createChatRoom(Map<String, Object> map) {
+        ChatRoom room = ChatRoom.builder()
+                .chatRoomName((String) map.get("name"))
+                .build();
+
+        List<Integer> userIdList = (List<Integer>) map.get("userIdList");
+
+        for (Integer userId : userIdList) {
+            User user = userRepository.findById(Long.valueOf(userId)).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
+            ChatRoomUser chatRoomUser = ChatRoomUser.builder()
+                    .chatRoom(room)
+                    .user(user)
+                    .build();
+
+            chatRoomUserRepository.save(chatRoomUser);
+        }
+
+        chatRoomRepository.save(room);
+        chatMessageRepository.save(ChatMessage.builder()
+                .chatRoom(room)
+                .user(null)
+                .message("채팅방이 생성되었습니다.")
+                .timestamp(LocalDateTime.now())
+                .build());
+
+        return room;
     }
 }
