aced47ca0f323a3d23bc6af70cf5459e30b91d9c
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-08-03 09:41:29 +0900

    Feature: 리뷰의 댓글을 작성하는 API - SubReviewDto를 작성 - 유저검증 로직

--- backend/src/main/java/com/ssafy/backend/controller/SubReviewController.java
+++ backend/src/main/java/com/ssafy/backend/controller/SubReviewController.java
@@ -1,10 +1,13 @@
 package com.ssafy.backend.controller;
 
+import com.ssafy.backend.dto.SubReviewDto;
 import com.ssafy.backend.service.SubReviewService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/subreview")
@@ -16,5 +19,13 @@ public class SubReviewController {
     @GetMapping("/list/{reviewId}")
     public ResponseEntity<?> getSubReviewList(@PathVariable Long reviewId) {
         return ResponseEntity.ok().body(subReviewService.getSubReviewList(reviewId));
+    }
+
+
+    // 리뷰의 댓글을 작성하는 API
+    @PostMapping("/guard/write/{reviewId}")
+    public ResponseEntity<?> writeSubReview(@PathVariable Long reviewId, @RequestBody SubReviewDto subReviewDto, HttpServletRequest request) {
+        String userId = (String) request.getAttribute("userId");
+        return ResponseEntity.ok().body(subReviewService.writeSubReview(reviewId, subReviewDto, Long.valueOf(userId)));
     }
 }
--- backend/src/main/java/com/ssafy/backend/service/SubReviewService.java
+++ backend/src/main/java/com/ssafy/backend/service/SubReviewService.java
@@ -1,7 +1,12 @@
 package com.ssafy.backend.service;
 
+import com.ssafy.backend.dto.SubReviewDto;
+import com.ssafy.backend.entity.DrinkReview;
 import com.ssafy.backend.entity.SubReview;
+import com.ssafy.backend.entity.User;
+import com.ssafy.backend.repository.DrinkReviewRepository;
 import com.ssafy.backend.repository.SubReviewRepository;
+import com.ssafy.backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,9 +16,29 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SubReviewService {
     private final SubReviewRepository subReviewRepository;
+    private final UserRepository userRepository;
+    private final DrinkReviewRepository drinkReviewRepository;
 
     // 리뷰의 댓글을 조회하는 API
     public List<SubReview> getSubReviewList(Long reviewId) {
         return subReviewRepository.findAllByDrinkReview_DrinkReviewId(reviewId);
+    }
+
+    // 리뷰의 댓글을 작성하는 API
+    public SubReview writeSubReview(Long reviewId, SubReviewDto subReviewDto, Long userId) {
+        DrinkReview drinkReview = drinkReviewRepository.findById(reviewId).orElseThrow(() -> new IllegalArgumentException("해당 리뷰가 존재하지 않습니다."));
+        User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("해당 유저가 존재하지 않습니다."));
+
+        // content가 비어있는지 확인
+        if (subReviewDto.getContent().isEmpty()) {
+            throw new IllegalArgumentException("댓글 내용이 비어있습니다.");
+        }
+
+        SubReview subReview = SubReview.builder()
+                .content(subReviewDto.getContent())
+                .drinkReview(drinkReview)
+                .user(user)
+                .build();
+        return subReviewRepository.save(subReview);
     }
 }
