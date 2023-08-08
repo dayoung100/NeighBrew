82bea9a8e1d0a7f311b92d8d6fa364f638071763
dayounglee <53906423+dayoung100@users.noreply.github.com>
2023-08-08 16:31:32 +0900

    Feat:유저 승인 거절 기능 구현

--- Front/src/UI/meet/MeetingMemberManage.tsx
+++ Front/src/UI/meet/MeetingMemberManage.tsx
@@ -4,11 +4,15 @@
 참여중인 인원, 신청한 인원을 보여주고 승인, 거절이 가능
 */
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Modal from "react-modal";
 import styled from "styled-components";
 import NavbarSimple from "../navbar/NavbarSimple";
 import PeopleNumInfo from "./PeopleNumInfo";
 import Footer from "../footer/Footer";
 import UserInfoItem from "../components/UserInfoItem";
+import { callApi } from "../../utils/api";
+import { MeetDetail, User } from "../../Type/types";
 
 const SubTitle = styled.div`
   font-size: 20px;
@@ -35,24 +39,78 @@ const NoBtn = styled(BtnSmall)`
   background: #f28f79;
 `;
 
-const MeetingMemberManage = () => {
-  const [memberList, setMemberList] = useState([
-    "주최자 이름",
-    "참여자1",
-    "참여자2",
-    "참여자3",
-  ]);
-  const [applicantList, setApplicantList] = useState([
-    "신청자1",
-    "신청자2",
-    "신청자3",
-  ]);
+const WhiteModal = {
+  content: {
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "15rem",
+    height: "6rem",
+    padding: "0.5rem 1rem",
+    borderRadius: "15px",
+    background: "white",
+    textAlign: "center",
+    fontFamily: "NanumSquareNeo",
+  },
+  overlay: {
+    background: "rgba(0, 0, 0, 0.5)",
+    zIndex: "11",
+  },
+};
 
-  const acceptHandler = (userName: string) => {
-    console.log("you accepted user : ", userName);
-  };
-  const refuseHandler = (userName: string) => {
-    console.log("you refused user : ", userName);
+const initialData: MeetDetail = {
+  meetDto: {
+    meetId: 0,
+    meetName: "",
+    description: "",
+    nowParticipants: 0,
+    maxParticipants: 0,
+    meetDate: "0000-01-01T00:00:00",
+    tagId: 0,
+    sido: "-",
+    gugun: "-",
+    dong: "-",
+    drink: {
+      degree: 0,
+      description: "",
+      drinkId: 0,
+      image: "",
+      name: "",
+      tagId: 0,
+    },
+    imgSrc: "",
+  },
+  users: [],
+  statuses: [],
+};
+
+const initialUser = {
+  userId: 0,
+  email: "",
+  nickname: "",
+  name: "",
+  liverPoint: 0,
+  profile: "",
+  follower: 0,
+  following: 0,
+};
+
+const MeetingMemberManage = () => {
+  const { meetId } = useParams(); //meetId는 라우터 링크에서 따오기
+  const [meetData, setMeetData] = useState<MeetDetail>(initialData);
+  const [memberList, setMemberList] = useState<User[]>([]);
+  const [applicantList, setApplicantList] = useState<User[]>([]);
+  const [modalOn, setModalOn] = useState(false);
+  const [targetUser, setTargetUser] = useState<User>(initialUser); //승인/거절할 유저
+  const [targetAction, setTargetAction] = useState(true); //승인:true, 거절:false
+  const [errorModalOn, setErrorModalOn] = useState(false);
+  const [errorModalMsg, setErrorModalMsg] = useState("");
+
+  const fetchMeetData = () => {
+    const promise = callApi("get", `api/meet/${meetId}`);
+    promise.then((res) => {
+      setMeetData(res.data);
+    });
   };
 
   useEffect(() => {
@@ -60,7 +118,39 @@ const MeetingMemberManage = () => {
       top: 0,
       behavior: "smooth",
     });
+    fetchMeetData();
   }, []);
+
+  useEffect(() => {
+    if (meetData === undefined) return;
+    //guest 세팅
+    let member = meetData.users.filter(
+      (user, index) => meetData.statuses[index] !== "APPLY"
+    );
+    setMemberList(member);
+    //apply 세팅
+    let applicants = meetData.users.filter(
+      (user, index) => meetData.statuses[index] === "APPLY"
+    );
+    setApplicantList(applicants);
+  }, [meetData]);
+
+  //수락/거절하기
+  const memberHandler = (user: User) => {
+    const promise = callApi("post", `api/meet/manage-user`, {
+      userId: user.userId,
+      meetId: parseInt(meetId),
+      applyResult: targetAction,
+    });
+    promise
+      .then(() => {
+        fetchMeetData();
+      })
+      .catch((e) => {
+        setErrorModalMsg(e.response.data);
+        setErrorModalOn(true);
+      });
+  };
 
   return (
     <div style={{ fontFamily: "JejuGothic" }}>
@@ -68,21 +158,24 @@ const MeetingMemberManage = () => {
         <NavbarSimple title="모임 관리" />
       </header>
       <div style={{ fontSize: "32px", marginTop: "1rem" }}>
-        모임의 제목이 들어갑니다
+        {meetData.meetDto.meetName}
       </div>
       <div style={{ marginBottom: "7rem" }}>
         <SubTitle style={{ display: "flex", alignItems: "center" }}>
           <div>참여중</div>
-          <PeopleNumInfo now={1} max={4} color="var(--c-black)" size={15} />
+          <PeopleNumInfo
+            now={meetData.meetDto.nowParticipants}
+            max={meetData.meetDto.maxParticipants}
+            color="var(--c-black)"
+            size={15}
+          />
         </SubTitle>
         <div style={{ margin: "0 1rem" }}>
           {memberList.map((member, index) => {
             return (
               <UserInfoItem
-                userId={1}
-                name={member}
-                intro="한 마디는 이렇게 저렇게 작성하기 한 마디는 이렇게 저렇게 작성하기"
-                imgSrc="/src/assets/tempgif.gif"
+                key={member.userId}
+                user={member}
                 isMaster={index === 0}
                 width={15}
               />
@@ -91,21 +184,29 @@ const MeetingMemberManage = () => {
         </div>
         <SubTitle style={{ textAlign: "left" }}>참여 신청</SubTitle>
         <div style={{ margin: "0 1rem" }}>
-          {applicantList.map((applicant, index) => {
+          {applicantList.map((applicant) => {
             return (
-              <div style={{ display: "flex", alignItems: "center" }}>
-                <UserInfoItem
-                  userId={1}
-                  name={applicant}
-                  intro="한 마디는 이렇게 저렇게 작성하기 한 마디는 이렇게 저렇게 작성하기"
-                  imgSrc="/src/assets/tempgif.gif"
-                  isMaster={false}
-                  width={13}
-                />
-                <OKBtn onClick={() => acceptHandler(applicant)}>
+              <div
+                key={applicant.userId}
+                style={{ display: "flex", alignItems: "center" }}
+              >
+                <UserInfoItem user={applicant} isMaster={false} width={13} />
+                <OKBtn
+                  onClick={() => {
+                    setTargetAction(true);
+                    setTargetUser(applicant);
+                    setModalOn(true);
+                  }}
+                >
                   <img src="/src/assets/checkButtonIcon.svg" />
                 </OKBtn>
-                <NoBtn onClick={() => refuseHandler(applicant)}>
+                <NoBtn
+                  onClick={() => {
+                    setTargetAction(false);
+                    setTargetUser(applicant);
+                    setModalOn(true);
+                  }}
+                >
                   <img src="/src/assets/XButtonIcon.svg" />
                 </NoBtn>
               </div>
@@ -113,6 +214,50 @@ const MeetingMemberManage = () => {
           })}
         </div>
       </div>
+      <Modal
+        isOpen={modalOn}
+        onRequestClose={() => setModalOn(false)}
+        style={WhiteModal}
+      >
+        <div style={{ padding: "1rem 0" }}>
+          유저 {targetUser.nickname}을/를 <br />
+          {targetAction ? "승인" : "거절"}
+          하시겠습니까?
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+          }}
+        >
+          <div
+            onClick={() => {
+              memberHandler(targetUser);
+              setModalOn(false);
+            }}
+            style={{ width: "7rem", padding: "0.5rem 0" }}
+          >
+            예
+          </div>
+          <div>|</div>
+          <div
+            onClick={() => {
+              setModalOn(false);
+            }}
+            style={{ width: "7rem", padding: "0.5rem 0" }}
+          >
+            아니오
+          </div>
+        </div>
+      </Modal>
+      <Modal
+        isOpen={errorModalOn}
+        onRequestClose={() => setErrorModalOn(false)}
+        style={WhiteModal}
+      >
+        {errorModalMsg}
+      </Modal>
       <Footer />
     </div>
   );
