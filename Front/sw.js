5072dadd498d6f73f5fde7b1e316cb361872b4cb
godzz733 <godzz733@naver.com>
2023-08-08 17:46:56 +0900

    FE:pushtest

--- Front/sw.js
+++ Front/sw.js
@@ -1,13 +1,23 @@
-self.addEventListener('install', e => {
-  console.log('[Service Worker] installed');
+self.addEventListener("install", e => {
+  console.log("[Service Worker] installed");
 });
 
 // activate event
-self.addEventListener('activate', e => {
-  console.log('[Service Worker] actived', e);
+self.addEventListener("activate", e => {
+  console.log("[Service Worker] actived", e);
 });
 
 // fetch event
-self.addEventListener('fetch', e => {
-  console.log('[Service Worker] fetched resource ' + e.request.url);
+self.addEventListener("fetch", e => {
+  console.log("[Service Worker] fetched resource " + e.request.url);
+});
+
+self.addEventListener("push", event => {
+  const options = {
+    body: event.data.text(),
+    icon: "path/to/icon.png", // 알림 아이콘 경로
+    badge: "path/to/badge.png", // 뱃지 아이콘 경로
+  };
+
+  event.waitUntil(self.registration.showNotification("알림 제목", options));
 });
