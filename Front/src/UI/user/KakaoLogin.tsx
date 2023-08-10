6956c99d14bb2adf4804bf78db25f25ec45824d9
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-08-10 16:40:36 +0900

    fix: git 충돌 해결

--- Front/src/UI/user/KakaoLogin.tsx
+++ Front/src/UI/user/KakaoLogin.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { callApi } from "../../utils/api";
 const KakaoLogin = () => {
   const navigate = useNavigate();
@@ -11,24 +11,20 @@ const KakaoLogin = () => {
       .post("http://localhost:8080/api/auth/kakao", {
         authorizationCode: code,
       })
-      .then(res => {
+      .then((res) => {
         localStorage.setItem("token", res.data.accessToken);
         localStorage.setItem("refreshToken", res.data.refreshToken);
       })
       .then(async () => {
-<<<<<<< Updated upstream
         await callApi("get", "/api/user/guard/myinfo")
-=======
-        await callApi("get", "http://localhost:8080/api/user/guard/myinfo")
->>>>>>> Stashed changes
-          .then(res => {
+          .then((res) => {
             localStorage.setItem("myId", JSON.stringify(res.data.userId));
             console.log(res.data);
           })
-          .catch(err => console.log(err));
+          .catch((err) => console.log(err));
         // await navigate("/meet");
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   };
