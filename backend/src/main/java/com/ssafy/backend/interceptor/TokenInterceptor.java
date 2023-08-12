dcf12d2a8e8b13f8835be0735c5c42485841bbee
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-08-13 08:09:46 +0900

    Feature: 유저 검증을 위해서 인터셉터에에서 헤더의 유저 아이디와 토큰이 일치하는지 확인하는 로직 추가

--- backend/src/main/java/com/ssafy/backend/interceptor/TokenInterceptor.java
+++ backend/src/main/java/com/ssafy/backend/interceptor/TokenInterceptor.java
@@ -11,25 +11,25 @@ public class TokenInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println("TokenInterceptor.preHandle");
         String header = request.getHeader("Authorization");
 
         if (header == null || !header.startsWith("Bearer ")) {
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid token format");
-            return false;
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "토큰이 존재하지 않습니다.");
         }
 
         String token = header.substring(7);
-        Claims claims;
-        try {
-            claims = JwtUtil.getClaims(token);
-        } catch (Exception e) {
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid token");
-            return false;
+        Claims claims = JwtUtil.getClaims(token);
+
+        String tokenUserId = claims.getSubject();
+
+        // 요청 헤더에서 "UserID"로 userId를 받아온다
+        String requestUserId = request.getHeader("UserID");
+
+        if (requestUserId == null || !requestUserId.equals(tokenUserId)) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "토큰의 유저 정보와 일치하지 않습니다.");
         }
 
-        String userId = claims.getSubject();
-        request.setAttribute("userId", userId);
+        request.setAttribute("userId", tokenUserId);
         return true;
     }
 }
