ef1173b91c4b088f5212cff6ede8b1eb18ab74b9
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-08-13 08:41:36 +0900

    Feature: 유저 검증을 interceptor가 아닌 filter에서 처리하도록 변경 - 유저가 로그인할 때 유저의 아이디를 함께 전달 - 유저의 데이터가 토큰의 유저와 일치하지 않는다면 에러발생

--- backend/src/main/java/com/ssafy/backend/config/WebMvcConfig.java
+++ backend/src/main/java/com/ssafy/backend/config/WebMvcConfig.java
@@ -1,20 +1,11 @@
 package com.ssafy.backend.config;
 
-import com.ssafy.backend.interceptor.TokenInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new TokenInterceptor())
-                .addPathPatterns("/**/guard/**");
-    }
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
